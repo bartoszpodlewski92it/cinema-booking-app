1ceafb56467e60a4d6f6a667a4fc6ff10920d2be
bartoszpodlewski92it <bartoszpodlewski92it@wp.pl>
2026-05-16 19:28:45 +0200

    Initial commit - baza pod system rezerwacji kinowej

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-slate-900 text-white p-6">
+      <h1 className="text-4xl font-bold text-blue-400 mb-2">
+        Tu powstanie moja apka do portfolio Frontend & QA!
+      </h1>
+      <p className="text-gray-400 text-lg">
+        Witaj w portfolio Frontend & QA! Tutaj powstanie aplikacja.
+      </p>
+    </div>
+  );
+}
